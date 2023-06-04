ff4e20176e3cece1f3d010b363e8a19c4dd59819
Shaugato Paroi <102578316+Shaugato@users.noreply.github.com>
2023-06-04 19:42:32 +1000

    Add files via upload

---
+++ 2.1/2.1Task_2022/2.1Task_2022/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using System;
+//using System.Collections.Generic;
+using SplashKitSDK;
+
+
+
+
+namespace ShapeDrawer
+{
+    public class Program
+    {
+
+        public static void Main()
+
+        {
+            Window window = new Window("Test Window", 800, 600);
+            window.Clear(Color.White);
+
+            SplashKit.Delay(5000);
+
+        }
+    }
+}
+
+
