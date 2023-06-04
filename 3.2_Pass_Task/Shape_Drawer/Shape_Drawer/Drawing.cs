97d10968cadad44139d2ffc01afd0a13e242bb44
Shaugato Paroi <102578316+Shaugato@users.noreply.github.com>
2023-06-04 23:51:17 +1000

    Add files via upload

---
+++ 3.2_Pass_Task/Shape_Drawer/Shape_Drawer/Drawing.cs
@@ -0,0 +1,124 @@
+﻿
+using System.Collections.Generic;
+using SplashKitSDK;
+
+namespace Shape_Drawer
+{
+    public class Drawing
+    {
+        // List of shapes and background color for the Drawing
+        private readonly List<Shape> _shapes;
+        private Color _background;
+
+        // Constructor that initializes the list of shapes and background color
+        public Drawing(Color background)
+        {
+            _shapes = new List<Shape>();
+            _background = background;
+        }
+
+        // Default constructor that initializes the list of shapes with a white background
+        public Drawing() : this(Color.White)
+        {
+        }
+
+        // Clears the screen and draws all the shapes in the list
+        public void Draw()
+        {
+            SplashKit.ClearScreen();
+            foreach (Shape shape in _shapes)
+            {
+                // Draws the shape
+                shape.Draw();
+            }
+
+        }
+
+        // Adds a shape to the list of shapes
+        public void AddShape(Shape shape)
+        {
+            // Adds the shape to the list of shapes
+            _shapes.Add(shape);
+        }
+
+        // Removes a shape from the list of shapes and from the display
+        public void DeleteShape(Shape s)
+        {
+            // Removes the shape from the list of shapes, causing it to disappear from the display
+            _shapes.Remove(s);
+        }
+
+        // Selects shapes at the specified point
+        public void SelectShapesAt(Point2D select_pt)
+        {
+            foreach (Shape s in _shapes)
+            {
+                // Selects the shape if it is at the specified point
+                if (s.IsAt(select_pt))
+                {
+                    s.Selected = true;
+                }
+                else
+                {
+                    s.Selected = false;
+                }
+            }
+        }
+
+        // Changes the color of the shape at the current mouse position
+        public void RandomColor()
+        {
+            Point2D mousePosition = SplashKit.MousePosition();
+
+            foreach (Shape shape in _shapes)
+            {
+                // Changes the color of the shape at the current mouse position to a random color
+                if (shape.IsAt(mousePosition))
+                {
+                    shape.Color = SplashKit.RandomRGBColor(255);
+                }
+            }
+        }
+
+        // Getter and setter for the background color
+        public Color Background
+        {
+            get
+            {
+                return _background;
+            }
+            set
+            {
+                _background = value;
+            }
+        }
+
+        // Returns the number of shapes in the list
+        public int ShapeCount
+        {
+            get { return _shapes.Count; }
+        }
+
+        // Returns a list of all the selected shapes
+        public List<Shape> SelectedShapes
+        {
+            get
+            {
+                List<Shape> result;
+                result = new List<Shape>();
+                foreach (Shape s in _shapes)
+                {
+                    // Adds the selected shape to the list of selected shapes
+                    if (s.Selected == true)
+                    {
+                        result.Add(s);
+                    }
+                }
+                return result;
+            }
+        }
+
+
+    }
+
+}
---
+++ 3.2_Pass_Task/Shape_Drawer/Shape_Drawer/Program.cs
@@ -0,0 +1,71 @@
+﻿
+using SplashKitSDK;
+
+namespace Shape_Drawer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Create a window named "Shape Drawer" with a size of 800x600
+            Window shapeDrawerWindow = new Window("Shape Drawer", 800, 600);
+            // Create a new Drawing object to hold the shapes
+            Drawing drawingObject = new Drawing();
+
+            // Enter a loop that handles user input
+            do
+            {
+                // Process events related to user input
+                SplashKit.ProcessEvents();
+
+                // Clear the screen to prepare for drawing shapes
+                SplashKit.ClearScreen();
+
+                // If the user clicks the left mouse button
+                if (SplashKit.MouseClicked(MouseButton.LeftButton))
+                {
+                    // Create a new Shape object at the position of the mouse click
+                    Shape myShape = new Shape();
+                    myShape.X = SplashKit.MouseX();
+                    myShape.Y = SplashKit.MouseY();
+
+                    // Add the new shape to the drawing object
+                    drawingObject.AddShape(myShape);
+                }
+
+                // If the user clicks the right mouse button
+                if (SplashKit.MouseClicked(MouseButton.RightButton))
+                {
+                    // Select the shape(s) at the position of the mouse click
+                    Point2D mousePosition = SplashKit.MousePosition();
+                    drawingObject.SelectShapesAt(mousePosition);
+                }
+
+                // If the user presses the spacebar
+                if (SplashKit.KeyTyped(KeyCode.SpaceKey))
+                {
+                    // Change the color of the selected shape(s)
+                    drawingObject.RandomColor();
+                }
+
+                // If the user presses the backspace key
+                if (SplashKit.KeyTyped(KeyCode.BackspaceKey))
+                {
+                    // Delete the selected shape(s)
+                    foreach (Shape selectedShape in drawingObject.SelectedShapes)
+                    {
+                        drawingObject.DeleteShape(selectedShape);
+                    }
+                }
+
+                // Draw all shapes in the drawing object
+                drawingObject.Draw();
+
+                // Refresh the screen to display changes
+                SplashKit.RefreshScreen();
+
+                // Continue the loop until the user closes the window
+            } while (!SplashKit.WindowCloseRequested("Shape Drawer"));
+        }
+    }
+}
